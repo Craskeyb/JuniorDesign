890e7f2a8e0ce1bf94bfe002b47ed64d0fcefa29
Craskeyb <craskeybt@gmail.com>
2023-07-23 18:12:40 -0400

    Included chess-board library for visual testing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import chess
 import chess.pgn
 from pgnReader import gameRead
+from chessboard import display
 
 
 print("Welcome to the Chess Recall Program!")
@@ -11,5 +12,18 @@ gameList, moveCounts = gameRead()
 
 board = chess.Board()
 
-print(gameList[0])
-print(moveCounts[0])
+moveArr = []
+for moves in gameList[0].mainline_moves():
+    moveArr.append(moves)
+    #print(board.board_fen())
+
+for i in range(0,25):
+    board.push(moveArr[i])
+
+valid_fen = board.board_fen()
+
+game_board = display.start()
+
+while True:
+    display.check_for_quit()
+    display.update(valid_fen, game_board)
